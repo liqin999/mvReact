e2751e8455aebbcdbca256ee4ade8dc07feac1d9
liqin <1483510623@qq.com>
2017-12-13 23:35:54 +0800

    jsx语法知识

--- HelloReact/src/app.js
+++ HelloReact/src/app.js
@@ -2,8 +2,20 @@ import React ,{Component} from 'react';
 import ReactDom from 'react-dom';
 require("./common/style/main.css");
 
+// jsx语法知识  使用babel知识 元素中的类 class - classname
+// jsx中的表达式 使用大括号的形式{}
+// 自定义的属性使用data-的形式
+let num = 888;
+let domem = <em>数量</em>;
+let w1 = "hello";
+
 ReactDom.render(
-	<div>Hello React,welcome back</div>,
+	<div>
+		<span className='col-red' data-msg='first'>{w1}</span>
+		<span>{num}</span>
+		{domem}
+		<span>world</span>
+	</div>,
 	document.getElementById("root")
 );
 
