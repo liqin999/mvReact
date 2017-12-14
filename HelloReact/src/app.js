fafac931b500b923ce9f53fdf564974e6d077071
liqin <1483510623@qq.com>
2017-12-14 21:22:53 +0800

    组件的嵌套形式

--- HelloReact/src/app.js
+++ HelloReact/src/app.js
@@ -1,21 +1,39 @@
 import React ,{Component} from 'react';
 import ReactDom from 'react-dom';
 require("./common/style/main.css");
-
+require("../semantic/dist/semantic.css");
 // jsx语法知识  使用babel知识 元素中的类 class - classname
 // jsx中的表达式 使用大括号的形式{}
 // 自定义的属性使用data-的形式
-let num = 888;
-let domem = <em>数量</em>;
-let w1 = "hello";
+
+class Nav extends Component{
+	render(){
+		return(
+			<div className="ui three item menu">
+			  <a className="active item">社论</a>
+			  <a className="item">Reviews</a>
+			  <a className="item">即将举办的活动</a>
+			</div>
+		)
+	}
+}
+
+class Body extends Component{
+	render(){
+		return (
+			<div>
+				<img className='img1' src={require("./common/img/128H.jpg")}/> 
+			</div>
+
+			)
+	}
+}
 
 ReactDom.render(
-	<div>
-		<span className='col-red' data-msg='first'>{w1}</span>
-		<span>{num}</span>
-		{domem}
-		<span>world</span>
-	</div>,
+	    <div className="ui container">
+ 			<Nav/>
+			<Body />
+	    </div>,
 	document.getElementById("root")
 );
 
--- HelloReact/webpack.config.js
+++ HelloReact/webpack.config.js
@@ -1,18 +1,20 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-	entry:[
-			'./src/app.js'
-		],
-	output:{
-		filename:'main.js',
-		path:path.resolve(__dirname,'dist/assets'),
-		publicPath:'/assets/'
-	},
-	module:{
-		rules:[
-			 {
+    entry: [
+        './src/app.js'
+    ],
+
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist/assets'),
+        publicPath: '/assets/'
+    },
+
+    module: {
+        rules: [
+            {
                 test: /\.js$/,
                 use: ['babel-loader'],
                 include:[
@@ -22,17 +24,43 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: ['url-loader?limit=8192'],
+            },
+            {
+                test: /\.(mp4|ogg|svg)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                use: ['url-loader?limit=10000&mimetype=application/font-woff']
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                use: ['url-loader?limit=10000&mimetype=application/octet-stream']
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: ['url-loader?limit=10000&mimetype=image/svg+xml']
             }
-		]
-	},
-	plugins:[
-		new webpack.HotModuleReplacementPlugin(),
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
-            filename: '../index.html',
+            filename: '../in.html',
             template: './src/index.html'
         })
-	],
-	 devtool: 'cheap-module-eval-source-map'
+    ],
+
+    devtool: 'cheap-module-eval-source-map'
 
 
 }
